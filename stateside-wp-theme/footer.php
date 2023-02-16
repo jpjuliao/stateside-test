eb286041c5f0e6ec5258f15a5524ad615dc29751
Juan Pablo Juliao <jpjuliao@gmail.com>
2023-02-16 16:15:48 -0500

    Install Wordpress with docker compose

---
+++ stateside-wp-theme/footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer
+ */
+
+?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- #content -->
+
+
+</div><!-- #page -->
+
+<script
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly"
+  defer
+></script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ stateside-wp-theme/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The main template file
+ */
+
+get_header(); ?>
+
+<div id="map"></div>
+
+<?php get_footer();
